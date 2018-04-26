a20b63f112048486ab0b67ebd645e49aae07df5a
db117 <z351622948@163.com>
2018-04-26 21:21:28 +0800

    ..

--- src/main/java/com/db117/adminstaging/modules/sys/controller/SysMenuController.java
+++ src/main/java/com/db117/adminstaging/modules/sys/controller/SysMenuController.java
@@ -1,6 +1,8 @@
 package com.db117.adminstaging.modules.sys.controller;
 
 
+import cn.hutool.core.util.StrUtil;
+import com.db117.adminstaging.common.Result;
 import com.db117.adminstaging.common.base.BaseController;
 import com.db117.adminstaging.common.vo.ZtreeVO;
 import com.db117.adminstaging.modules.sys.entity.SysMenu;
@@ -56,10 +58,76 @@ public class SysMenuController extends BaseController {
         return "sys/menu/add";
     }
 
+    /**
+     * 根据父级id获取下级列表
+     */
     @PostMapping(value = "findByPid")
     @ResponseBody
     public List<SysMenu> findByPid(String pid) {
         return sysMenuService.findByPid(pid);
+    }
+
+
+    /**
+     * 进行添加
+     *
+     * @param sysMenu 菜单类
+     * @param dirId   目录id
+     * @param menuId  菜单id
+     */
+    @PostMapping(value = "add")
+    @ResponseBody
+    public Result add(SysMenu sysMenu, String dirId, String menuId) {
+        if (StrUtil.isBlank(sysMenu.getName())) {
+            return Result.getFailure("名称不能为空");
+        }
+
+        String type = sysMenu.getType();
+        if ("0".equals(type)) {
+            sysMenu.setParentId("1");
+            sysMenu.setParentIds("0,1");
+            sysMenuService.insertAllColumn(sysMenu);
+            return Result.getSuccess("添加成功");
+        }
+
+        if (StrUtil.isBlank(sysMenu.getHref())) {
+            return Result.getFailure("链接不能为空");
+        }
+        if ("1".equals(type)) {
+            sysMenu.setParentId(dirId);
+            sysMenu.setParentIds("0,1," + dirId);
+        }
+
+        if ("2".equals(type)) {
+            sysMenu.setParentId(menuId);
+            sysMenu.setParentIds("0,1," + dirId + "," + menuId);
+        }
+        sysMenuService.insertAllColumn(sysMenu);
+        return Result.getSuccess("添加成功");
+    }
+
+    /**
+     * 进入表单界面
+     */
+    @GetMapping(value = "form")
+    public String form(String id, Model model) {
+        SysMenu sysMenu = sysMenuService.selectById(id);
+        model.addAttribute("sysMenu", sysMenu);
+
+        //如果是菜单
+        String type = sysMenu.getType();
+        if ("1".equals(type)) {
+            List<SysMenu> dirMenuList = sysMenuService.findAllDir();
+            model.addAttribute("dirMenuList", dirMenuList);
+        }
+
+        //如果是按钮
+        if ("2".equals(type)) {
+            String ppid = sysMenu.getParentIds();
+//            String[] strings=ppid
+//            List<SysMenu> menuList=sysMenuService
+        }
+        return "sys/menu/form";
     }
 }
 
